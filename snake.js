fe7008078e9b385d02e8077487c254166d7f827d
Kemps Medeiros <kempsmm@gmail.com>
2022-10-21 16:35:50 -0300

    feat: snakejs

--- snake.js
+++ snake.js
@@ -1,18 +1,19 @@
+import { getInputDirection } from './input.js';
+
 //how many times snake move per second
 export const SNAKE_SPEED = 2;
 const snakeBody = [
-    {x: 10, y: 11},
-    {x: 11, y: 11},
-    {x: 12, y: 11},
+    {x: 11, y: 11}
 ]
 
 export function update() {
+    const inputDirection = getInputDirection();
     for(let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = { ...snakeBody[i] }
     }
 
-    // snakeBody[0].x += 0
-    // snakeBody[0].y += 1
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
