30e0e93354dc3244f869b78073ed16dadfad6760
Kemps Medeiros <kempsmm@gmail.com>
2022-10-21 15:57:48 -0300

    feat: creating snakejs

---
+++ snake.js
@@ -0,0 +1,26 @@
+//how many times snake move per second
+export const SNAKE_SPEED = 2;
+const snakeBody = [
+    {x: 10, y: 11},
+    {x: 11, y: 11},
+    {x: 12, y: 11},
+]
+
+export function update() {
+    for(let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = { ...snakeBody[i] }
+    }
+
+    // snakeBody[0].x += 0
+    // snakeBody[0].y += 1
+}
+
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = segment.y;
+        snakeElement.style.gridColumnStart = segment.x;
+        snakeElement.classList.add('snake');
+        gameBoard.appendChild(snakeElement);
+    })
+}
