a731bf790a9fe7df594b4c2fe451a7e4405c98fa
Kemps Medeiros <kempsmm@gmail.com>
2022-10-24 08:42:09 -0300

    fix: inputjs

--- input.js
+++ input.js
@@ -1,17 +1,22 @@
 let inputDirection = {x: 0, y: 0}
+let lastInputDirection = {x: 0, y: 0}
 
 window.addEventListener('keydown', e => {
     switch (e.key) {
         case 'ArrowUp':
+            if(lastInputDirection.y !== 0) break
             inputDirection = {x: 0, y: -1}
             break
         case 'ArrowDown':
+            if(lastInputDirection.y !== 0) break
             inputDirection = {x: 0, y: 1}
             break
         case 'ArrowLeft':
+            if(lastInputDirection.x !== 0) break
             inputDirection = {x: -1, y: 0}
             break
         case 'ArrowRight':
+            if(lastInputDirection.x !== 0) break
             inputDirection = {x: 1, y: 0}
             break
 
@@ -19,5 +24,6 @@ window.addEventListener('keydown', e => {
 })
 
 export function getInputDirection() {
+    lastInputDirection = inputDirection
     return inputDirection;
 }
