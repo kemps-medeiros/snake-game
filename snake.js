1cd065e56c433133b93ef4a2ac884596c430d364
Kemps Medeiros <kempsmm@gmail.com>
2022-10-24 08:42:57 -0300

    feat: adding newfeatures to snake

--- snake.js
+++ snake.js
@@ -1,12 +1,14 @@
 import { getInputDirection } from './input.js';
 
 //how many times snake move per second
-export const SNAKE_SPEED = 2;
+export const SNAKE_SPEED = 5;
 const snakeBody = [
     {x: 11, y: 11}
-]
+];
+let newSegments = 0;
 
 export function update() {
+    addSegments()
     const inputDirection = getInputDirection();
     for(let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = { ...snakeBody[i] }
@@ -24,4 +26,35 @@ export function draw(gameBoard) {
         snakeElement.classList.add('snake');
         gameBoard.appendChild(snakeElement);
     })
+}
+
+export function expandSnake(amount) {
+    newSegments += amount
+}
+
+export function onSnake(position, {ignoredHead = false} = {}) {
+    return snakeBody.some((segment, index) => {
+        if(ignoredHead && index === 0) return false
+        return equalPosition(segment, position)
+    })
+}
+
+function equalPosition(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push({ ...snakeBody[snakeBody.length -1]})
+    }
+
+    newSegments = 0;
+}
+
+export function getSnakeHead() {
+    return snakeBody[0];
+}
+
+export function snakeIntersection() {
+    return onSnake(snakeBody[0], {ignoredHead: true})
 }
