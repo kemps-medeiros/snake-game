c31359cab4dca8862b76faaa93fcf63ecfa2011a
Kemps Medeiros <kempsmm@gmail.com>
2022-10-24 08:43:14 -0300

    feat: implements new feaatures

--- game.js
+++ game.js
@@ -1,9 +1,16 @@
-import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from './snake.js';
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection } from './snake.js';
+import { update as updateFood, draw as drawFood } from './food.js';
+import { outsiderGrid } from './grid.js';
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
+    if(gameOver) {
+       return alert('you lose')
+    }
+
     window.requestAnimationFrame(main)
 
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
@@ -21,9 +28,16 @@ window.requestAnimationFrame(main);
 
 function update() {
     updateSnake()
+    updateFood()
+    checkDeath()
 }
 
 function draw() {
     gameBoard.innerHTML = ' '
     drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsiderGrid(getSnakeHead()) ||snakeIntersection();
 }
