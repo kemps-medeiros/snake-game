974383064817072b64d9a1b66bcfd520a7b1d39c
Kemps Medeiros <kempsmm@gmail.com>
2022-10-24 09:24:04 -0300

    feat: finishing the game

--- game.js
+++ game.js
@@ -8,7 +8,10 @@ const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
     if(gameOver) {
-       return alert('you lose')
+       if(confirm('You lost. Press ok to restart')) {
+        window.location = '/'
+       }
+       return
     }
 
     window.requestAnimationFrame(main)
